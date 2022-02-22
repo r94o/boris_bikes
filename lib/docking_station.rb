411f182c8a07a0256d9969a88673512bcb222b40
Rob Oman <rob.oman@icloud.com>
2022-02-22 17:37:58 +0000

    Add return bike tests and methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,25 @@
 require 'bike'
 
 class DockingStation
+
+  def initialize
+    @bike_rack = []
+  end
+
+
   def release_bike
     return Bike.new
   end
 
-  def return_bike
+  def bike_rack
+    return @bike_rack
   end
-  
+
+  def return_bike(bike)
+    @bike_rack.push(bike)
+
+    return @bike_rack.length
+  end
+
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,15 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  it 'responds to method release bike' do
+  it 'responds to method release_bike' do
     expect(subject).to respond_to(:release_bike)
   end
 
-  it "checks that a bike is released" do
+  it 'responds to method return_bike' do
+    expect(subject).to respond_to(:return_bike)
+  end
+
+  it "checks that a bike class is released" do
     expect(subject.release_bike).to be_an_instance_of(Bike)
   end
 
@@ -14,5 +18,11 @@ describe DockingStation do
     bike = Bike.new
     expect(bike.working?).to eq true
   end
-  
+
+  it "checks that when the bike is returned" do
+    bike = Bike.new
+    num_bikes = subject.bike_rack.length
+    expect(subject.return_bike(bike)).to eq (num_bikes + 1)
+  end
+
 end
