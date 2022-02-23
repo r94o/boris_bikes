13e95e85d71ab0b27c8de74944d2eef50ec6cb5e
Rob Oman <rob.oman@icloud.com>
2022-02-23 14:50:01 +0000

    Add more tests and tested for errors when bike_rack is empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,25 +1,24 @@
 require 'bike'
 
 class DockingStation
+  attr_accessor :bike_rack
 
   def initialize
     @bike_rack = []
   end
 
-
   def release_bike
-    return Bike.new
-  end
-
-  def bike_rack
-    return @bike_rack
+    unless bike_rack.empty?
+      return Bike.new
+    else
+      raise IndexError.new
+    end
   end
 
   def return_bike(bike)
     @bike_rack.push(bike)
-
+    puts "The bike has been returned"
     return @bike_rack.length
   end
 
 end
-
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,16 +2,13 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+  
   it 'responds to method release_bike' do
     expect(subject).to respond_to(:release_bike)
   end
 
   it 'responds to method return_bike' do
     expect(subject).to respond_to(:return_bike)
-  end
-
-  it "checks that a bike class is released" do
-    expect(subject.release_bike).to be_an_instance_of(Bike)
   end
 
   it "checks bike is working" do
@@ -25,4 +22,14 @@ describe DockingStation do
     expect(subject.return_bike(bike)).to eq (num_bikes + 1)
   end
 
+  it "does not release a bike when the bike rack is empty" do
+    subject.bike_rack= []
+    expect{ subject.release_bike }.to raise_error(IndexError)
+  end
+
+  it "does release a bike when the bike rack has at least 1 bike in" do
+    subject.bike_rack = [Bike.new]
+    expect(subject.release_bike).to be_an_instance_of(Bike)
+  end
 end
+
